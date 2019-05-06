e4c9b5f6218fe774db6576d1729ab5a9a04ff8da
Edgar Gomez <egomez.info@gmail.com>
2019-05-06 16:19:02 +0200

    aggiunto test model (solo per repo tests)

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from "./event-handler";
 export * from "./interfaces";
 export * from "./layout-builder";
 export * from "./layout-data-source";
+export * from "./test-model";
---
+++ src/test-model.ts
@@ -0,0 +1,6 @@
+// only for test purposes
+export class TestModel {
+  sayHi(){
+    return "hello!";
+  }
+}
