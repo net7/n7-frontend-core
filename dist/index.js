0ce18877d0b5f0daaad228a83c455c96eb77909a
Edgar Gomez <gomez@netseven.it>
2022-02-08 16:59:12 +0100

    ignoring dist folder

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-export * from "./data-source";
-export * from "./event-handler";
-export * from "./interfaces";
-export * from "./layout-builder";
-export * from "./layout-data-source";
-export * from "./translate";
-export * from "./analytics";
